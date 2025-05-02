6b5d190ea51dea8d141ba9001455eba18692322e
Solomon Preston <chezman9001@gmail.com>
2025-05-02 16:11:48 -0500

    Player/Camera Movement
    Player jump reverted, wall jump in development.
    Camera follows player currently

--- TBD/Assets/Scripts/PlayerMovement.cs
+++ TBD/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
+    [SerializeField] private float horizontalJumpPower;
     [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D body;
     private BoxCollider2D boxCollider;
+    private float horizontalInput;
+    private float wallJumpCooldown;
     private Animator anim;
-    private bool grounded;
     private bool freezeRotation = true;
 
 
@@ -21,24 +23,43 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        // Controlled by a and d or arrowLeft and arrowRight
-        body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
+        
 
         // Flip player for left-right movement
         if(horizontalInput > 0.01f)
         {
             transform.localScale = new Vector3(0.5f, 0.5f, 0);
-        }else if (horizontalInput < -0.01f)
+        }
+        else if (horizontalInput < -0.01f)
         {
             transform.localScale = new Vector3(-0.5f, 0.5f, 0);
         }
 
-        // Jump
-        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && grounded)
+        if(wallJumpCooldown > 0.2f)
         {
-            Jump();
+            if(onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.linearVelocity = Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 2.5f;
+            }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
+                Jump();
+            }
+
+            // Controlled by a and d or arrowLeft and arrowRight
+            body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
         }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
+        
 
         // Dash
         if(Input.GetKey(KeyCode.Space))
@@ -56,27 +77,41 @@ public class PlayerMovement : MonoBehaviour
         }
 
         anim.SetBool("walk", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
     }
 
     private bool isGrounded()
     {
+        // detect whether there is a ground layer below player
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        // detect whether there is a ground layer below player
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, 
+            new Vector2(transform.localScale.x, 0), 0.1f, groundLayer);
         return raycastHit.collider != null;
     }
 
     private void Jump()
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x, jumpSpeed);
-        anim.SetTrigger("jump");
-        grounded = false;
+        if(isGrounded())
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpSpeed);
+            anim.SetTrigger("jump");
+        } 
+        else if(onWall())
+        {
+            wallJumpCooldown = 0;
+
+            body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, jumpSpeed);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
+        
     }
 }
---
+++ TBD/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    private Transform currentCheckpoint;
+    
+    private void Respawn()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
